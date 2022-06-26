253384f1a863444f3ac1a7d7368f60ccf8a1248c
stolomeo <tolomeosebastian@gmail.com>
2022-06-26 14:57:07 -0400

    Added FavoriteInput tests

---
+++ src/FavoriteInput.tsx
@@ -0,0 +1,15 @@
+import { ChangeEvent } from "react";
+type Props = {
+  onChange: (onChange: any) => void;
+  id: undefined;
+};
+export default function FavoriteInput({ onChange: onInputChange, id }: Props) {
+  const inputHandler = (e: ChangeEvent) => onInputChange(e.target.value);
+
+  return (
+    <label htmlFor={id}>
+      What is your favorite wild animal?
+      <input id={id} onChange={inputHandler} />
+    </label>
+  );
+}
